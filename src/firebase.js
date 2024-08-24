c4c9541034d09ea43d1358d63842c75792c55af0
maxkaygee <maxkaygee@gmail.com>
2024-08-24 07:18:25 -0500

    commented out analytics cuz kept breaking app

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from "firebase/analytics"; [dont need yet ig]
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,6 +23,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app); [same thing]
 
 export { db, auth };
